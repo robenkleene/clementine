2a405587bb50e21fa095916cf84e63a01a97db8b
Roben Kleene <contact@robenkleene.com>
2023-08-01 06:06:45 -0400

    Extract values to randomize

--- javascript/randomize.js
+++ javascript/randomize.js
@@ -19,25 +19,27 @@ function msg_float(value) {
 
 function pitch(_) {
   var arr = arrayfromargs(messagename, arguments);
-  var randomizedValues = randomizeValues(arr.slice(2));
-  // arr.splice(2, randomizedValues.length, ...randomizeValues);
+  var values = arr.slice(2)
+  log(values);
   outlet(0, arr.join(" "));
 }
 
 function velocity(_) {
   var arr = arrayfromargs(messagename, arguments);
-  var randomizedValues = randomizeValues(arr.slice(2));
-  // arr.splice(2, randomizedValues.length, ...randomizeValues);
+  var values = arr.slice(2)
+  log(values);
   outlet(0, arr.join(" "));
 }
 
 function duration(_) {
   var arr = arrayfromargs(messagename, arguments);
-  var randomizedValues = randomizeValues(arr.slice(2));
-  // arr.splice(2, randomizedValues.length, ...randomizeValues);
+  var values = arr.slice(2)
+  log(values);
   outlet(0, arr.join(" "));
 }
 
-function randomizeValues(arr) {
-  return [];
+// TODO: Remove debugging code
+function log(obj) {
+  // `post` logs to console
+  post(JSON.stringify(obj));
 }
