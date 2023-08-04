27f18f49f79f84aa4a109d6782e07a457df69055
Roben Kleene <contact@robenkleene.com>
2023-08-04 05:57:15 -0400

    Work on randomize order

--- javascript/randomize.js
+++ javascript/randomize.js
@@ -11,10 +11,14 @@ INLET_VELOCITY = 4;
 INLET_DURATION = 5;
 
 // Store input
-var values = [[], 0, 0, 0, 0, 0];
+let input = [[], 0, 0, 0, 0, 0];
+
+function getRandomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min) + min) + 1;
+}
 
 function msg_float(value) {
-  values[inlet] = value;
+  input[inlet] = value;
 }
 
 // TODO:
@@ -23,9 +27,25 @@ function msg_float(value) {
 // Order: The note gets swapped with a random later note
 // Repeat: The note replaces the next note
 function pitch(_) {
-  var arr = arrayfromargs(messagename, arguments);
-  var values = arr.slice(2)
-  log(values);
+  let arr = arrayfromargs(messagename, arguments);
+  let values = arr.slice(2)
+  const repeat = input[INLET_REPEAT];
+  const order = input[INLET_ORDER];
+  for (let i = 0; i < values.length; i++) { 
+    if (Math.random() < order) {
+      if (i + 1 < values.length) {
+        const newIndex = getRandomBetween(i + 1, values.length);
+        const curr = values[newIndex];
+        values[newIndex] = values[i];
+        values[i] = curr;
+      }
+    }
+    if (Math.random() < repeat) {
+      if (i + 1 < values.length) {
+        values[i + 1] = values[i];
+      }
+    }
+  }
   outlet(0, arr.join(" "));
 }
 
@@ -33,8 +53,8 @@ function pitch(_) {
 // velocity `18` with a factor of `1` means `0-36`
 // velocity `127` with a factor of `1` means `0-127`
 function velocity(_) {
-  var arr = arrayfromargs(messagename, arguments);
-  var values = arr.slice(2)
+  let arr = arrayfromargs(messagename, arguments);
+  let values = arr.slice(2)
   log(values);
   outlet(0, arr.join(" "));
 }
@@ -42,8 +62,8 @@ function velocity(_) {
 // Same as velocity, but we have fixed values:
 // 7.5 15 30 60 120 240 480 960
 function duration(_) {
-  var arr = arrayfromargs(messagename, arguments);
-  var values = arr.slice(2)
+  let arr = arrayfromargs(messagename, arguments);
+  let values = arr.slice(2)
   log(values);
   outlet(0, arr.join(" "));
 }
