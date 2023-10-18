6123289fce22964ab22f65c7bb239f1b38917c9b
Roben Kleene <contact@robenkleene.com>
2023-10-18 05:36:27 -0400

    Fix output

--- misc/scripts/dump_scales.sh
+++ misc/scripts/dump_scales.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 cd "$(dirname "$0")" || exit 1
-jq 'keys_unsorted | join(", ")' ../../javascript/scales.json
+jq --raw-output 'keys_unsorted | join(", ")' ../../javascript/scales.json
