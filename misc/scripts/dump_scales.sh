64a8adcb0cf565e4cddac036b3ed9ac077257452
Roben Kleene <contact@robenkleene.com>
2025-11-27 06:23:18 -0500

    Switch to package

--- misc/scripts/dump_scales.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd "$(dirname "$0")" || exit 1
-jq --raw-output 'keys_unsorted | join(" ")' ../../javascript/clementine_scales.json
---
+++ scripts/dump_scales.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")" || exit 1
+jq --raw-output 'keys_unsorted | join(" ")' ../Clementine/javascript/clementine_scales.json
